3669f71cbf9fd2dab1b462fec53bc59181faf83c
seona.bang <sit.or.seona@gmail.com>
2023-04-03 23:31:38 +0900

    add: Store & Reducer

--- vanilla-redux/README.md
+++ vanilla-redux/README.md
@@ -1,1 +1,16 @@
 # Vanilla Redux
+
+## 1. Store and Reducer
+
+### Store란?
+
+- state(변경이 일어나는 data)를 저장할 수 있는 장소
+- createStore 라는 함수를 사용
+- 파라미터로 reducer를 입력하지 않으면 에러 발생
+- 객체로 dispatch, getState, replaceReducer, subscribe 메서드가 내장되어 있음
+
+### Reducer란?
+
+- data를 변경하는 **함수**
+- return 값이 app의 data가 됨
+- 하나의 reducer에 하나의 data
--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,24 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => {
+  return state;
 };
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
