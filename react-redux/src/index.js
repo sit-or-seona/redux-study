84a4f039796c0d90b2751b9d25995cf26288e7fd
seona.bang <sit.or.seona@gmail.com>
2023-04-08 23:33:22 +0900

    add: Connecting the Store

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
