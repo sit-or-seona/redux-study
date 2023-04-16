dcd9de460c89c0ba7202c2a466828152c0e035a8
seona.bang <sit.or.seona@gmail.com>
2023-04-16 15:47:32 +0900

    add: mapDispatchToProps

--- react-redux/README.md
+++ react-redux/README.md
@@ -6,6 +6,11 @@
   - store로부터 state를 받아옴
 - 2개의 아규먼트를 가짐
   - state와 dispatch 중에 고를 수 있기 때문
+
+### 1. mapStateToProps
+
+- store로부터 state를 받아 컴포넌트에 prop으로 전달
+- 컴포넌트에서 export default 뒤에 connect(빈함수) (컴포넌트)
   ```js
   function mapStateToProps(state, ownProps?)
   /*
@@ -14,8 +19,13 @@
   (react-router로부터 받은 props와 mapStateToProps에서 return한 값이 찍힘)
    */
   ```
-- 컴포넌트에서 export default 뒤에 connect(빈함수) (컴포넌트)
 
-### 1. mapStateToProps
+### 2. mapDispatchToProps
 
-- store로부터 state를 받아 컴포넌트에 prop으로 전달
+- mapState는 필요하지 않고 dispatch만 필요한 경우, null 할당
+  ```js
+  export default connect(null, mapDispatchToProps);
+  ```
+- 컴포넌트로부터 store의 reducer에게 dispatch함
+
+=> 위 2개의 함수로 컴포넌트에서 dispatch와 actionCreators를 처리
--- react-redux/src/routes/Home.js
+++ react-redux/src/routes/Home.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -9,6 +10,7 @@ function Home({ toDos }) {
   function onSubmit(e) {
     e.preventDefault();
     setText("");
+    addToDo(text);
   }
   return (
     <>
@@ -26,4 +28,10 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
