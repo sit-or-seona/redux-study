7cd6dbcd3517bd5443bbd370b5930d3d3fd86d8a
seona.bang <sit.or.seona@gmail.com>
2023-06-23 15:40:47 +0900

    add: recoil 02_BasicTutorial Todo Intro

---
+++ recoiljs.org/02_BasicTutorial/todo/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Recoil Example</h1>
+      <h2>Learn recoill with simple todo list app</h2>
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
